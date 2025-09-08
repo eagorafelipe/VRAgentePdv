4d4e745ada1736649f85264cb8930e707d9dd1d4
eagorafelipe <eagorafelipe+github@gmail.com>
2025-09-08 19:11:24 -0300

    VAP-001
    - Configurações para Multiplatform

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,21 +1,79 @@
 plugins {
-    kotlin("jvm") version "2.2.0"
+    kotlin("multiplatform") version "1.9.20"
+    kotlin("plugin.serialization") version "1.9.20"
 }
 
 group = "br.com.vrsoftware"
-version = "1.0-SNAPSHOT"
+version = "1.0.0"
 
 repositories {
     mavenCentral()
 }
 
-dependencies {
-    testImplementation(kotlin("test"))
+kotlin {
+
+    linuxX64("linux") {
+        binaries {
+            executable {
+                entryPoint = "main"
+                baseName = "vragentepdv-linux"
+            }
+        }
+    }
+
+    mingwX64("windows") {
+        binaries {
+            executable {
+                entryPoint = "main"
+                baseName = "vragentepdv-windows"
+            }
+        }
+    }
+
+    sourceSets {
+        val commonMain by getting {
+            dependencies {
+                implementation("org.jetbrains.kotlinx:kotlinx-coroutines-core:1.7.3")
+                implementation("org.jetbrains.kotlinx:kotlinx-serialization-json:1.6.0")
+                implementation("org.jetbrains.kotlinx:kotlinx-datetime:0.4.1")
+            }
+        }
+
+        val commonTest by getting {
+            dependencies {
+                implementation(kotlin("test"))
+            }
+        }
+
+        val linuxMain by getting {
+            dependencies {
+            }
+        }
+
+        val windowsMain by getting {
+            dependencies {
+            }
+        }
+    }
 }
 
-tasks.test {
-    useJUnitPlatform()
+tasks.register("buildAll") {
+    dependsOn("linuxX64Binaries", "mingwX64Binaries")
+    description = "Gerando executáveis para Linux e Windows"
+    group = "build"
 }
-kotlin {
-    jvmToolchain(21)
+
+tasks.register<Zip>("packageRelease") {
+    dependsOn("buildAll")
+    archiveFileName.set("salt-installer-${version}.zip")
+    destinationDirectory.set(file("$buildDir/releases"))
+
+    from("build/bin/linux/releaseExecutable") {
+        into("linux")
+    }
+    from("build/bin/windows/releaseExecutable") {
+        into("windows")
+    }
+    from("README.md")
+    from("LICENSE")
 }
